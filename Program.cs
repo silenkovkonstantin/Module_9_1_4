1db011ac05fa3cc55ef0bebb1919cce3392883a0
silenkovkonstantin <silenkov@inbox.ru>
2023-02-26 09:52:30 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+
+namespace Module_9_1_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exception exception = new("Что-то пошло не так");
+            exception.HelpLink = "www.yandex.ru";
+
+        }
+    }
+}
